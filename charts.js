6f56a271efac263fca7179baf4455e0a0961ab99
Sadatxcv <sadatxcv@gmail.com>
2022-10-05 15:07:59 +0600

    first commit

---
+++ charts.js
@@ -0,0 +1,28 @@
+
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "pie",
+  data: {
+    labels: ["Blue", "Green", "Purple", "Purple"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
